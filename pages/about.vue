8d4bfff40e631c9e06c43e0a5d4613ba75623170
胡旷杰 <754432607@qq.com>
2020-09-22 11:25:21 +0800

    'nuxt.js初始化'

---
+++ pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+      <h1>about 页面</h1>
+      <nuxt-link :to="{name:'index',params:{
+        xxid:'9527'
+      }}">回首页</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <ul>
+        <li> <nuxt-link :to="{name:'about'}">about</nuxt-link> </li>
+        <li> <nuxt-link :to="{name:'news'}">news</nuxt-link> </li>
+      </ul>
+      <h1 class="title">
+        nuxt_demo {{ $route.params.xxid }}
+      </h1>
+      <div class="links">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
